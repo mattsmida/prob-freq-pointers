dc7f01bd2508c9e220cf3e18bc58c8917b7daa0b
Nathan Rydel <nathanrydel@gmail.com>
2023-11-10 17:38:04 -0700

    averagePair is passing all testing

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,32 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+"use strict";
+
+/**
+ * Return a boolean check on if a targetAvg can be achieved in a array of numbers
+ * @param {number[]} nums - A sorted array of numbers
+ * @param {number} targetAvg - The value that we want to look for
+ * @return {Boolean} - The check on if targetAvg is achieveable by averaging pairs
+ *  in the nums array
+ * @example ([1, 2, 3], 2.5) -> true
+ */
+
+function averagePair(nums, targetAvg) {
+  if (nums.length === 0) return false;
+
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start < end) {
+    const avg = (nums[start] + nums[end]) / 2;
+
+    if (avg === targetAvg) {
+      return true;
+    } else if (avg < targetAvg) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+
+  return false;
 }
 
