4dafaed2bcbc9428c209e047b3107b75e7d51402
Matt Smida <mcs@m4t7.com>
2023-11-10 19:06:34 -0500

    canConstructWord is passing all tests

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,51 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict"
+
+/** This function check to see if *word* can be built from *letters*.
+ *  It takes in two strings (word and letters), and outputs a boolean.
+ *  f('aa', 'abc') --> false
+ */
+function canConstructWord(word, letters) {
+  // Make two frequency counters, one for words, one for letters
+  // Compare the values of wordFreqs and letterFreqs
+
+  if (word.length > letters.length) return false;
+
+  const wordFreqs = makeFreqCounter(word);
+  const letterFreqs = makeFreqCounter(letters);
+
+  // For each letter in word, make sure it appears in letterFreq
+  // Return false if not.
+
+  for (let char of word) {
+    if (!(char in letterFreqs)) {
+      return false;
+    }
+  }
+
+  // Loop through wordFreqs and make sure that the values for each key
+  // are less than or equal to the values in letterFreq.
+
+  for (let char in wordFreqs) {
+    if (wordFreqs[char] > letterFreqs[char]) {
+      return false;
+    }
+  }
+
+  return true;
+
+}
+
+/** Makes a frequency counter object by taking a string and returning
+ *  the object as key: char, value: number of occurences.
+ */
+function makeFreqCounter(str) {
+  const freqObj = {};
+  for (let char of str) {
+    if (freqObj[char] === undefined) {
+      freqObj[char] = 1;
+    } else {
+      freqObj[char]++;
+    }
+  }
+  return freqObj;
 }
